30ce51be540136fb7f98fd7f60875303efab854e
Elizabeth Larson <elarso2@uw.edu>
2022-04-30 15:47:52 -0700

    writing questions, reformatting classes, adding in some small functions

--- index.js
+++ index.js
@@ -2,11 +2,50 @@ const inquirer = require("inquirer");
 const jes = require("jest");
 const fs = require("fs");
 
-//Basic Employee Class
-class Employee {
-  constructor(name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-  }
-}
+const questionsEmployee = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is their employee ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is their email?",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is their role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+];
+
+const questionMan = [
+  {
+    type: "input",
+    name: "officeNum",
+    message: "What is the team manager's office number?",
+  },
+];
+
+const questionIntern = [
+  {
+    type: "input",
+    name: "ghProfile",
+    message: "What is this intern's GitHub username?",
+  },
+];
+
+const questionStu = [
+  {
+    type: "input",
+    name: "stuSchool",
+    message: "What school does this student attend?",
+  },
+];
